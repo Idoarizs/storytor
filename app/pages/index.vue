5d439fd87725a2fc98997954d3ea0c94ef149803
Aodi Saputra <aodirizky05@gmail.com>
2025-09-07 14:03:19 +0700

    adding landing page

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// custom components
+import Hero from "./Hero.vue";
+import HowItWorks from "./HowItWorks.vue";
+import CTA from "./CTA.vue";
+</script>
+
+<template>
+  <Hero />
+  <HowItWorks />
+  <CTA />
+</template>
