1405794cf93944162225338ab8609d42c6ee7b72
Aodi Saputra <aodirizky05@gmail.com>
2025-09-07 14:29:16 +0700

    adding icon, modify web config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  app: {
+    head: {
+      title: "Storytor – Minimalist Space for Data Storytellers",
+      meta: [
+        {
+          name: "description",
+          content:
+            "A minimalist space for data-based storytellers and writers to draft ideas and collect references — free forever.",
+        },
+        {
+          name: "keywords",
+          content:
+            "storytelling, writing, data-driven, minimalist, free forever, tiptap based tool, note-taking, idea drafting, reference collection",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   css: ["@/assets/css/global.css"],
 
